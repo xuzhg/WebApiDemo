93c277dc20cdd11627abd78df16a57573fe78a5d
Sam Xu <saxu@microsoft.com>
2016-03-07 15:59:17 +0900

    add web api routing

---
+++ WebApiDemo/WebApiDemo/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.OData;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class CustomersController : ODataController
+    {
+        [EnableQuery]
+        public IHttpActionResult Get()
+        {
+            return Ok(DataSource.Customers);
+        }
+
+        [EnableQuery]
+        public IHttpActionResult Get(int key)
+        {
+            return Ok(DataSource.Customers.FirstOrDefault(c => c.Id == key));
+        }
+    }
+}
